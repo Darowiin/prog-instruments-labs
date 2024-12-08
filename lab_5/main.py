3975d41341962df3798f8037e413e362ac780dc2
Darowiin <vlad.tsunenko.2004@mail.ru>
2024-12-08 18:00:14 +0400

    refactor main for tests

--- lab_5/main.py
+++ lab_5/main.py
@@ -5,7 +5,10 @@ from HybridEncryption import HybridEncryption
 from SymmetricCryptography import SymmetricCryptography
 from AsymmetricCryptography import AsymmetricCryptography
 
-def main():
+def parse_arguments():
+    """
+    Parse command-line arguments.
+    """
     parser = argparse.ArgumentParser(description="Single entry point for key generation, encryption, and decryption.")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-gen', '--generation',
@@ -53,22 +56,45 @@ def main():
                         default=os.path.join('decrypted_text.txt'),
                         help='Path to the decrypted text file (default: decrypted_text.txt).')
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def create_hybrid_encryption(args):
+    """
+    Create and return a HybridEncryption instance based on the provided arguments.
+    """
     symmetric_crypto = SymmetricCryptography(args.key_length)
-    asymmetric_crypto = AsymmetricCryptography(args.private_key,args.public_key)
-    hybrid_encryption = HybridEncryption(args.text_file,
-                                        args.symmetric_key_file,args.encrypted_text_file,
-                                        args.decrypted_text_file,symmetric_crypto,asymmetric_crypto)
-    
-    
+    asymmetric_crypto = AsymmetricCryptography(args.private_key, args.public_key)
+    return HybridEncryption(
+        args.text_file,
+        args.symmetric_key_file,
+        args.encrypted_text_file,
+        args.decrypted_text_file,
+        symmetric_crypto,
+        asymmetric_crypto,
+    )
+
+
+def run_mode(hybrid_encryption, args):
+    """
+    Run the specified mode based on command-line arguments.
+    """
     if args.generation:
         hybrid_encryption.generate_keys()
-
     elif args.encryption:
         hybrid_encryption.encrypt_text()
-
     elif args.decryption:
         hybrid_encryption.decrypt_text()
+
+
+def main():
+    """
+    Main entry point for the script.
+    """
+    args = parse_arguments()
+    hybrid_encryption = create_hybrid_encryption(args)
+    run_mode(hybrid_encryption, args)
+
 
 if __name__ == "__main__":
     main()
